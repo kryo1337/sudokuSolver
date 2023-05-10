cf570090f84d922f50b42084b292a7f512bc89c9
kryo1337 <kryo1337@gmail.com>
2023-05-10 10:51:51 +0100

    fully working solver

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ board = [
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
@@ -22,8 +23,49 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
+
 def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)
+    return None
+
+
+def valid(board, num, pos):
+    # checking row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+    # checking column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    # checking box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y * 3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, len(board) + 1):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+print_board(board)
+solve(board)
+print("\n Solved")
+print_board(board)
